810bec664a3b4a64f50b23dbb794e5b9e8b57919
Yovangga Anandhika <dhikaprivate@gmail.com>
2021-03-27 15:53:39 +0800

    Initial commit

---
+++ WPF/Akuntaris/splashScreen.xaml.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Akuntaris
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+
+        DispatcherTimer dt = new DispatcherTimer();
+
+        public SplashScreen()
+        {
+            InitializeComponent();
+
+            dt.Tick += new EventHandler(dt_Tick);
+            dt.Interval = new TimeSpan(0, 0, 2);
+            dt.Start();
+        }
+
+        private void dt_Tick(object sender, EventArgs e)
+        {
+            Home home = new Home();
+            home.Show();
+
+            dt.Stop();
+            this.Close();
+        }
+    }
+}
---
+++ WPF/AkuntarisCore/AkuntarisCore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AkuntarisCore
+{
+    public class AkuntarisCore
+    {
+        public void Base(String text)
+        {
+
+        }
+    }
+}
